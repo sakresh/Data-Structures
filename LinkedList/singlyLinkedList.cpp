5e064f496b1e4d1c5fa1edb39aee0431bc34243e
name <sarkeshnandu@gmail.com>
2024-03-04 16:28:10 +0530

    Insert at the beginning function added for the linked list

--- LinkedList/singlyLinkedList.cpp
+++ LinkedList/singlyLinkedList.cpp
@@ -12,9 +12,37 @@ class Node{
 };
 
 class LinkedList{
+    private:
+        Node* head;
+    public:
+        LinkedList(){
+            this->head = nullptr;
+        }
+
+        // Inserting a node at the beginning
+        void insertAtBeginning(int data){
+            Node *newNode = new Node(data);
+            newNode->next = head;
+            head = newNode;
+        }
+
+        // Inserting a node at the end
+        void insertAtEnd(int data){
+            Node *newNode = new Node(data);
+            while(head != nullptr){
+                head = head->next;
+            }
+        }
 
 };
 
 int main(int argc, char** argv){
-    cout<<"Creating Linked List";
+    LinkedList ll;
+
+    ll.insertAtBeginning(30);
+    ll.insertAtBeginning(20);
+    ll.insertAtBeginning(10);
+
+
+
 }
